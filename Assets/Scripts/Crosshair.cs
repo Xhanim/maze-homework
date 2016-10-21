164e8034d77170dccdda18dfe0e4c1481e2605a4
xhanim <boscanp.luis@gmail.com>
2016-10-20 20:24:24 -0400

    Improved fireball aim and added new crosshair textures (not implemented yet)

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -5,27 +5,28 @@ public class Crosshair : MonoBehaviour
 {
     public Texture2D crosshairTexture;
     public Texture2D crosshairActiveTexture;
-    public Rect position;
     private GauntletController gauntlet;
 
     void Start()
     {
         gauntlet = GetComponent<GauntletController>();
-        position = new Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - crosshairTexture.height) / 2, crosshairTexture.width, crosshairTexture.height);
     }
 
     void OnGUI()
     {
         UnityEngine.Cursor.lockState = CursorLockMode.Confined;
         UnityEngine.Cursor.visible = false;
-        
-        if(gauntlet.getTargetAnalyzer().InSight())
+
+        Texture2D activeTexture;
+        if (gauntlet.getTargetAnalyzer().InSight())
         {
-            GUI.DrawTexture(position, crosshairActiveTexture);
+            activeTexture = crosshairActiveTexture;
         }
         else
         {
-            GUI.DrawTexture(position, crosshairTexture);
+            activeTexture = crosshairTexture;
         }
+        Rect position = new Rect((Screen.width - activeTexture.width) / 2, (Screen.height - activeTexture.height) / 2, activeTexture.width, activeTexture.height);
+        GUI.DrawTexture(position, activeTexture);
     }
 }
--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -5,6 +5,8 @@ using System;
 public class Fireball : MonoBehaviour, TargetAnalyzer {
     public GameObject prefab;
     public GameObject spawn;
+    public float distance = 10;
+    public float force = 2500;
 
     public bool InSight()
     {
@@ -19,8 +21,12 @@ public class Fireball : MonoBehaviour, TargetAnalyzer {
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
+            // Make spawn look at camera position
+            Vector3 cameraPosition = Camera.main.transform.position;
+            cameraPosition += Camera.main.transform.forward * distance;
+            spawn.transform.LookAt(cameraPosition);
             GameObject ballInstance = Instantiate(prefab, spawn.transform.position, spawn.transform.rotation) as GameObject;
-            ballInstance.GetComponent<Rigidbody>().AddForce(spawn.transform.forward * 2500);
+            ballInstance.GetComponent<Rigidbody>().AddForce(spawn.transform.forward * force);
         }
     }
 }
