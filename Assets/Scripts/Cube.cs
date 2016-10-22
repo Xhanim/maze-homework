b7fbf726b9898a25d6502f0c51f1bb49aa6f492e
Viktor Briceno <viktor.briceno@gmail.com>
2016-10-22 14:13:55 -0400

    Created grab intro scene and improved the cube/swtich scripts.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+    public enum CubeType {
+        Orange, Purple
+    };
+    public CubeType type;
+    
+}
--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,18 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Switch : MonoBehaviour {
-    public bool ignoreMaterial;
+    public Cube.CubeType type;
     /**
      * List to hold activators to call when this switch is "activated".
      * */
     public List<BaseActivator> activators;
-    // to check material matches if activated
-    private Renderer materialTile;
     private GameObject attachedChild;
 
     void Start()
     {
-        materialTile = transform.GetChild(0).GetComponent<Renderer>();
     }
 
     /**
@@ -26,14 +23,15 @@ public class Switch : MonoBehaviour {
         {
             child.transform.parent = gameObject.transform;
             child.transform.position = gameObject.transform.position;
-            child.transform.localPosition = new Vector3(0, 5f, 0);
-            child.transform.localRotation = gameObject.transform.rotation;
+            child.transform.localPosition = new Vector3(0, 0, 0);
+            child.transform.localRotation = new Quaternion(0, 0, 0, 0);// new Vector3(0, 0, 0); gameObject.transform.rotation;
 
             attachedChild = child;
 
+            Debug.Log("cube material: " + child.GetComponent<Renderer>().material.name);
             // call activators
             // check material name? if it has one and there is no match then dont activate!
-            if (ignoreMaterial || (!ignoreMaterial && materialTile.material.name.Equals(child.GetComponent<Renderer>().material.name)))
+            if (type == child.GetComponent<Cube>().type)
             {
                 foreach (BaseActivator activator in activators)
                 {
