e22e1537cfbace6c8275902cf255030f589a11f5
Luis Boscan <boscanp.luis@gmail.com>
2016-10-23 20:56:06 -0400

    Added ShootableSwitch script
    Switch that you fire at and it activates things

---
+++ Assets/Scripts/ShootableSwitch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class ShootableSwitch : MonoBehaviour, Health {
+
+    public List<BaseActivator> activators;
+
+    public void TakeDamage(GameObject origin, int damage)
+    {
+        foreach (BaseActivator activator in activators)
+        {
+            activator.Activate(origin);
+        }
+    }
+}
