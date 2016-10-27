0c51c0ded974c36207b1daffdcdde15ee470b737
xhanim <boscanp.luis@gmail.com>
2016-10-26 21:26:08 -0400

    Added new functionality to MovePattern script
    - Move only into the specified axis.
    - Reset loops.
    - Clear path list.
    - Add new path elements.

--- Assets/Scripts/MovePattern.cs
+++ Assets/Scripts/MovePattern.cs
@@ -21,41 +21,72 @@ public class MovePattern : MonoBehaviour
     public bool affectY = true;
     public bool affectZ = true;
     private float currentTime = 0;
-    private Vector3 lastPosition;
+    private Vector3 lastObjectPosition;
     private int currentIndex = 0;
     private int loopCount;
-    private int direction = 1;
+    private int indexTraversalDirection = 1;
     private Rigidbody rigidBody;
     private List<GameObject> currentCollisions = new List<GameObject>();
     private bool waitingSwift;
     private float swiftCounter;
+    private Vector3 temp;
+    /**
+     * Contains the original position of this 
+     * entity before the loop cycle begins 
+     * */
+    private GameObject originObject;
+    private bool dirty = true;
 
     void Start()
     {
-        if (includeOriginalPosition)
+        rigidBody = GetComponent<Rigidbody>();
+        originObject = new GameObject();
+        originObject.name = name + " original position";
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        currentIndex = 0;
+        currentTime = 0;
+        loopCount = 0;
+        swiftCounter = 0;
+        waitingSwift = false;
+        indexTraversalDirection = 1;
+        if (dirty)
         {
-            GameObject originObject = new GameObject();
             originObject.transform.position = transform.position;
-            positionObjects.Insert(0, originObject);
+            /* if true this will insert the original position 
+             * of this object as the first point in the path */
+            if (includeOriginalPosition)
+            {
+                positionObjects.Insert(0, originObject);
+            }
         }
+        /* Set the current position to the position 
+         * of the first element in the list */
         if (positionObjects.Count > 0)
         {
             transform.position = positionObjects[0].transform.position;
             currentIndex = 1;
         }
-        lastPosition = transform.position;
-        rigidBody = GetComponent<Rigidbody>();
+        lastObjectPosition = transform.position;
+        dirty = false;
     }
 
     void FixedUpdate()
     {
-        if (positionObjects.Count == 0 || loopCount > 0 && loopMode == LoopMode.NONE || loopCount > 0 && positionObjects.Count == 1)
+        if (dirty)
+        {
+            Reset();
+        }
+        if (IsIndexOutOfBounds())
         {
             return;
         }
         if (waitingSwift)
         {
-            swiftCounter += Time.deltaTime;
+            swiftCounter += Time.fixedDeltaTime;
             if (swiftCounter >= swiftDelay)
             {
                 waitingSwift = false;
@@ -63,22 +94,30 @@ public class MovePattern : MonoBehaviour
             }
             return;
         }
-        GameObject currentObject = positionObjects[currentIndex];
+        MoveToNextPosition();
+    }
+
+    private void MoveToNextPosition()
+    {
         currentTime += Time.fixedDeltaTime;
         float step = currentTime / timePerPosition;
-        Vector3 newPosition = getNewPosition(currentObject.transform.position, step);
-        Vector3 currentPosition = transform.position;
-        updatePosition(newPosition);
-        if (newPosition == currentObject.transform.position)
+        GameObject nextPositionObject = positionObjects[currentIndex];
+        // Get new position between old point and new point
+        Vector3 newPosition = GetNewPosition(nextPositionObject.transform.position, step);
+        SetPosition(newPosition);
+        // Change target if we have reached our current target
+        if (EqualPositions(newPosition, nextPositionObject.transform.position))
         {
             currentTime = 0;
-            lastPosition = newPosition;
-            handleTargetChange();
+            lastObjectPosition = newPosition;
+            HandleTargetChange();
         }
     }
 
-    private void updatePosition(Vector3 newPosition)
+    private void SetPosition(Vector3 position)
     {
+        // Apply only enabled axis to the new position
+        Vector3 newPosition = ResolveConditionalPosition(transform.position, position);
         if (rigidBody != null)
         {
             rigidBody.MovePosition(newPosition);
@@ -89,68 +128,116 @@ public class MovePattern : MonoBehaviour
         }
     }
 
-    private void handleTargetChange()
+    private bool IsIndexOutOfBounds()
     {
-        currentIndex = currentIndex + 1 * direction;
-        if (currentIndex > positionObjects.Count - 1 || currentIndex < 0)
+        return currentIndex > positionObjects.Count - 1 || currentIndex < 0;
+    }
+
+    private void HandleTargetChange()
+    {
+        currentIndex = currentIndex + 1 * indexTraversalDirection;
+        if (IsIndexOutOfBounds())
         {
-            loopCount++;
-            if (loopMode == LoopMode.RING)
-            {
+            HandleLoopEnd();
+        }
+    }
+
+    /**
+     * Handles the actions to perform when a loop cycle ends.
+     */
+    private void HandleLoopEnd()
+    {
+        loopCount++;
+        switch (loopMode)
+        {
+            case LoopMode.RING:
                 currentIndex = 0;
-            }
-            else if (loopMode == LoopMode.REVERSE)
-            {
-                direction *= -1;
-                currentIndex = currentIndex + 2 * direction;
+                break;
+            case LoopMode.REVERSE:
+                indexTraversalDirection *= -1;
+                currentIndex = currentIndex + 2 * indexTraversalDirection;
                 waitingSwift = true;
-            } else if (loopMode == LoopMode.RESTART)
-            {
+                break;
+            case LoopMode.RESTART:
                 currentIndex = 1;
                 Vector3 newPosition = positionObjects[0].transform.position;
-                updatePosition(newPosition);
-                lastPosition = newPosition;
-                // hardcoding this part because of time
-                TrailRenderer trailRenderer = GetComponent<TrailRenderer>();
-                if (trailRenderer != null)
-                {
-                    trailRenderer.Clear();
-                }
-            }
+                SetPosition(newPosition);
+                lastObjectPosition = newPosition;
+                OnLoopRestart();
+                break;
         }
     }
 
-    private Vector3 getNewPosition(Vector3 nextPosition, float step)
+    /**
+     * Handles the actions to perform when a loop is restarted.
+     */
+    private void OnLoopRestart()
     {
-        Vector3 newPosition;
-        if (step >= 1)
+        // hardcoding this part because of time
+        TrailRenderer trailRenderer = GetComponent<TrailRenderer>();
+        if (trailRenderer != null)
         {
-            newPosition = nextPosition;
+            trailRenderer.Clear();
         }
-        else
-        {
-            newPosition = Vector3.Lerp(lastPosition, nextPosition, step);
-        }
-        return newPosition;
     }
 
-    private Vector3 resolveConditionalPosition(Vector3 position)
+    /**
+     * Returns the position to apply between the last object position 
+     * and the next object position, depending on the given step (between 0 and 1).
+     */
+    private Vector3 GetNewPosition(Vector3 nextObjectPosition, float step)
     {
-        Vector3 conditionalPosition = Vector3.zero;
-        // shitty if
-        if (affectX)
-        {
-            conditionalPosition.x = position.x;
-        }
-        if (affectY)
-        {
-            conditionalPosition.y = position.y;
-        }
-        if (affectZ)
-        {
-            conditionalPosition.z = position.z;
-        }
+        return Vector3.Lerp(lastObjectPosition, nextObjectPosition, step);
+    }
 
-        return conditionalPosition;
+    /**
+     * Compares two positions and returns true if they are equal. 
+     * Only the axis active in the pattern movement are used in the comparison.
+     */
+    private bool EqualPositions(Vector3 position1, Vector3 position2)
+    {
+        bool equalX = affectX ? position1.x == position2.x : true;
+        bool equalY = affectY ? position1.y == position2.y : true;
+        bool equalZ = affectZ ? position1.z == position2.z : true;
+        return equalX && equalY && equalZ;
+    }
+
+    /**
+     * Returns a position set to the newPosition only for the axis that are 
+     * enabled in the pattern movement, for the ones that are not enabled 
+     * they are set to the current position.
+     */
+    private Vector3 ResolveConditionalPosition(Vector3 currentPosition, Vector3 newPosition)
+    {
+        temp.x = affectX ? newPosition.x : currentPosition.x;
+        temp.y = affectY ? newPosition.y : currentPosition.y;
+        temp.z = affectZ ? newPosition.z : currentPosition.z;
+        return temp;
+    }
+
+    /**
+     * Restarts this component and starts the loops from the begining.
+     */
+    public void Reset()
+    {
+        Initialize();
+        OnLoopRestart();
+    }
+
+    /**
+     * Removes all the points of the path list.
+     */
+    public void ClearPathPoints()
+    {
+        positionObjects.Clear();
+        dirty = true;
+    }
+
+    /**
+     * Adds a GameObject to the end of the list of path points.
+     */
+    public void AddPathPoint(GameObject gameObject)
+    {
+        positionObjects.Add(gameObject);
     }
 }
