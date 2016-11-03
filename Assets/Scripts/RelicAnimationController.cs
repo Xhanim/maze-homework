4e472e50e1ae2b3c17845e2c648de2bfcdd69beb
Luis Boscan <boscanp.luis@gmail.com>
2016-11-03 02:59:55 -0400

    Added transition for when the relic animation is completed

---
+++ Assets/Scripts/RelicAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RelicAnimationController : MonoBehaviour {
+
+    public Transition transition;
+    public Animator animator;
+    private bool transitionActivated;
+    
+	void Update () {
+	    if (!transitionActivated && animator.GetBool("transition"))
+        {
+            transitionActivated = true;
+            transition.StartTransition();
+        }
+	}
+}
---
+++ Assets/Scripts/TransitionStateMachine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionStateMachine : StateMachineBehaviour {
+
+    public float completionPercentage;
+    private bool transitionActivated;
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!transitionActivated && stateInfo.normalizedTime >= completionPercentage)
+        {
+            transitionActivated = true;
+            animator.SetTrigger("transition");
+        }
+    }
+}
