f5bd8c69d0f04010f1a8763a017c7bf6ef92354a
Luis Boscan <boscanp.luis@gmail.com>
2016-11-03 01:43:22 -0400

    Added transitions to gaunlet, relic 1 and relic 2

---
+++ Assets/Scripts/Activators/TransitionActivator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TransitionActivator : BaseActivator {
+
+    public Transition transition;
+
+    public override void Activate(GameObject trigger)
+    {
+        transition.StartTransition();
+    }
+
+    public override void Desactivate()
+    {
+    }
+}
---
+++ Assets/Scripts/Transitions/GameObjectActivatorTransitionHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectActivatorTransitionHandler : BaseTransitionHandler
+{
+    public GameObjectActivator activator;
+
+    public override void OnWaitingBegin(Transition transition)
+    {
+        activator.Activate(gameObject);
+    }
+}
