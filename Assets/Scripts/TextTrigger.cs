912ea5a200bf5a7249f79f0f8d009d924b17dff6
Viktor Briceno <viktor.briceno@gmail.com>
2016-11-03 16:57:41 -0400

    Added text triggers.

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour {
+    public string text;
+    public float time;
+    public Text label;
+    private float currentTime;
+    private bool active;
+
+    void FixedUpdate()
+    {
+        if (active)
+        {
+            currentTime += Time.fixedDeltaTime;
+            if(currentTime >= time)
+            {
+                label.text = "";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Avatar")
+        {
+            label.text = text;
+            active = true;
+        }
+    }
+}
