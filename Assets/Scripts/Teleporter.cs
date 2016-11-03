9a70eec425b4d5280c3d9bcbb52b68d429f77294
Viktor Briceno <viktor.briceno@gmail.com>
2016-11-02 21:46:51 -0400

    Animations kinda fixed

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -8,17 +8,34 @@ public class Teleporter : MonoBehaviour, ITargetAnalyzer
     public float maxDistance = 13;
     private TeleportWaypoint teleportWaypoint;
     private Camera camera;
+    // the animator controller from the gauntlet
+    private Animator animator;
+    private bool justShoot;
+
+    void Awake()
+    {
+        // this is shitty code #shame
+        animator = GetComponent<GauntletController>().gauntletModel.GetComponent<Animator>();
+    }
 
     void Start()
     {
         camera = GetComponentInChildren<Camera>();
     }
 
-    void Update () {
+    void Update ()
+    {
+        if (justShoot)
+        {
+            justShoot = false;
+            animator.SetBool("teleportShoot", false);
+        }
         UpdateDetectedWaypoint();
         if (Input.GetMouseButtonDown(0) && teleportWaypoint != null)
         {
             teleportWaypoint.TeleportObject(gameObject);
+            animator.SetBool("teleportShoot", true);
+            justShoot = true;
         }
     }
 
@@ -35,6 +52,16 @@ public class Teleporter : MonoBehaviour, ITargetAnalyzer
         {
             teleportWaypoint = null;
         }
+    }
+
+    void OnEnable()
+    {
+        animator.SetBool("teleportPower", true);
+    }
+
+    void OnDisable()
+    {
+        animator.SetBool("teleportPower", false);
     }
 
     public bool InSight()
